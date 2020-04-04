2533bbb4b238a72f1e705ed5acd4d448d24924e5
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-05 04:04:41 +0530

    Done

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("./"));
+  require("dotenv").config();
 
-// const client = require("twilio")(accountSid, authTOken);
+const client = require("twilio")(accountSid, authTOken);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -19,45 +20,30 @@ app.listen(PORT, () => {
 app.get("/", (req,res) => {
   res.sendFile(path.join(__dirname,'/index.html'))
 })
-// app.post("/", async (req, res) => {
-//   const quantity = req.body.quantity;
-//   const foodItem = req.body.foodItem;
-//   const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
-//   numbers.forEach(sendMsg);
-//   function sendMsg(){
-//   client.messages
-//     .create({
-//       to: "+919088720168",
-//       from: process.env.MY_TWILIO_NUMBER,
-//       body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
-//     })
-//     .then((message) => console.log(message));
-//   }
-//   console.log(quantity);
-// });
-
-if(process.env.NODE_ENV !== 'production'){
-
-  require("dotenv").config();
-  const client = require("twilio")(accountSid, authTOken);
-  app.use(express.static('./'));
 app.post("/", async (req, res) => {
   const quantity = req.body.quantity;
   const foodItem = req.body.foodItem;
   const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
   numbers.forEach(sendMsg);
   function sendMsg(){
-  client.messages
-    .create({
-      to: "+919088720168",
-      from: process.env.MY_TWILIO_NUMBER,
-      body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
-    })
-    .then((message) => console.log(message));
+  // client.messages
+  //   .create({
+  //     to: "+919088720168",
+  //     from: process.env.MY_TWILIO_NUMBER,
+  //     body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
+  //   })
+  //   .then((message) => console.log(message));
   }
   console.log(quantity);
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/index.html"));
-})}
+if(process.env.NODE_ENV === 'production'){
+
+  // require("dotenv").config();
+//   const client = require("twilio")(accountSid, authTOken);
+//   app.use(express.static('./'));
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "/index.html"));
+// })}
+}
