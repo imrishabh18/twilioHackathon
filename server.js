3cce21b8fc259bdc250ecc28f865897717a0c69a
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-04 21:05:08 +0530

    Happy Fridge first push

---
+++ server.js
@@ -0,0 +1,33 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authTOken = process.env.TWILIO_AUTH_TOKEN;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./"));
+
+require("dotenv").config();
+const client = require("twilio")(accountSid, authTOken);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+app.post("/", (req, res) => {
+  const quantity = req.body.quantity;
+  const foodItem = req.body.foodItem;
+  const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
+  numbers.forEach(sendMsg);
+  function sendMsg(){
+  client.messages
+    .create({
+      to: "+919088720168",
+      from: process.env.MY_TWILIO_NUMBER,
+      body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
+    })
+    .then((message) => console.log(message));
+  }
+});
