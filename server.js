07d9fc7ad90e385fe35c0423b6027cfa89a468f0
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-05 03:22:50 +0530

    Changes

--- server.js
+++ server.js
@@ -16,18 +16,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const quantity = req.body.quantity;
   const foodItem = req.body.foodItem;
   const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
   numbers.forEach(sendMsg);
   function sendMsg(){
-  client.messages
-    .create({
-      to: "+919088720168",
-      from: process.env.MY_TWILIO_NUMBER,
-      body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
-    })
-    .then((message) => console.log(message));
+  // client.messages
+  //   .create({
+  //     to: "+919088720168",
+  //     from: process.env.MY_TWILIO_NUMBER,
+  //     body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
+  //   })
+  //   .then((message) => console.log(message));
   }
+  console.log(quantity);
 });
