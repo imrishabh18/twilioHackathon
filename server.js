e578c5c76518ab1f4c5d3d69045d9ddf074906fa
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-05 03:35:43 +0530

    removed static files

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const authTOken = process.env.TWILIO_AUTH_TOKEN;
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("./"));
+// app.use(express.static("./"));
 
 require("dotenv").config();
 const client = require("twilio")(accountSid, authTOken);
@@ -16,6 +17,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+app.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname,'/index.html'))
+})
 app.post("/", async (req, res) => {
   const quantity = req.body.quantity;
   const foodItem = req.body.foodItem;
