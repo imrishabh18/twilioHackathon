e2ffeafca6c9154a1ea3e4ac6cd0d17716b50a8c
Rishabh Gupta <belikerishabh@gmail.com>
2020-04-05 17:06:39 +0530

    sfS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // require("dotenv").config();
 
-// const client = require("twilio")(accountSid, authTOken);
+const client = require("twilio")(accountSid, authTOken);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
@@ -18,17 +18,17 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
   const quantity = req.body.quantity;
   const foodItem = req.body.foodItem;
-  // const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
-  // numbers.forEach(sendMsg);
-  // function sendMsg(){
-  // client.messages
-    // .create({
-    //   to: "+919088720168",
-    //   from: process.env.MY_TWILIO_NUMBER,
-    //   body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
-    // })
-    // .then((message) => console.log(message));
-  // }
+  const numbers = ["+919088720168","+918088540168","+919578720168","+918088720168","+919088440168","+919088720168","+919038720168","+919048720167"];
+  numbers.forEach(sendMsg);
+  function sendMsg(){
+  client.messages
+    .create({
+      to: "+919088720168",
+      from: process.env.MY_TWILIO_NUMBER,
+      body: `Food item entered in refrigator is ${foodItem} which is sufficient for ${quantity} people`,
+    })
+    .then((message) => console.log(message));
+  }
   console.log(quantity);
 });
 const PORT = process.env.PORT || 5000;
